d0ca9ad9039ddb62bb94f7bd761633d430e0490c
Michał Kołodziejczyk <miko@wp.pl>
2021-04-08 19:01:02 +0200

    Upgrade version to v0.1.13

--- cmd/root.go
+++ cmd/root.go
@@ -22,7 +22,7 @@ var (
 	rootCmd = &cobra.Command{
 		Use:   "dmt",
 		Short: "DGraph migration tool",
-		Long:  `DGraph migration tool v0.1.10.`,
+		Long:  `DGraph migration tool v0.1.13.`,
 	}
 )
 
