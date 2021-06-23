8f17b5edb9568b14c18091df63fab00901a65fdd
Michał Kołodziejczyk <miko@wp.pl>
2021-06-23 14:54:34 +0200

    Up version: v.0.2.5

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-TAG=${TAG:-v0.2.4}
+TAG=${TAG:-v0.2.5}
 docker build -t miko/dmt .
 docker tag miko/dmt miko/dmt:${TAG}
 
--- cmd/root.go
+++ cmd/root.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	envPrefix = "DMT"
-	VER       = "v0.2.4"
+	VER       = "v0.2.5"
 )
 
 var (
